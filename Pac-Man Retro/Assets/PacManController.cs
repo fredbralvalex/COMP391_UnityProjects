3e424e3bac443326ba0d9bf5b46beafc7df9dead
Frederico Alexandre <fred.bralvalex@gmail.com>
2018-03-05 18:49:35 -0500

    finishing week 8
    Pacman Medium Done

--- Pac-Man Retro/Assets/PacManController.cs
+++ Pac-Man Retro/Assets/PacManController.cs
@@ -8,7 +8,9 @@ public class PacManController : MonoBehaviour {
     //Speed of PacMan and Phantoms
     public const float SPEED_CONSTANT = 1f;
 
-    public float speed = 1;
+    public enum Direction { Stoped, Up, Down, Left, Right };
+
+    public float speed = 0.05f;
 
     private Rigidbody2D pacman;
 
@@ -21,7 +23,59 @@ public class PacManController : MonoBehaviour {
         //get move directions
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        pacman.AddForce(movement * speed * SPEED_CONSTANT);
+        Direction direction = GetDirection(moveHorizontal, moveVertical);
+        float horizontal = 0;
+        float vertical = 0;
+        float rotation = 0;
+        if (direction == Direction.Down)
+        {
+            vertical = -1;
+            rotation = 180;
+        } else if (direction == Direction.Up)
+        {
+            vertical = 1;
+            rotation = 0;
+        }
+        else if (direction == Direction.Left)
+        {
+            horizontal = -1;
+            rotation = 90;
+        }
+        else if (direction == Direction.Right)
+        {
+            horizontal = 1;
+            rotation = -90;
+        }
+
+        Vector2 movement = new Vector2(pacman.position.x + horizontal * speed,
+            pacman.position.y + vertical * speed);
+        //pacman.AddForce(movement * speed * SPEED_CONSTANT);
+        pacman.MovePosition(movement);
+        pacman.MoveRotation(rotation);
+        //transform.Translate(movement);
+    }
+
+    Direction GetDirection(float moveHorizontal, float moveVertical)
+    {
+        if (moveHorizontal > 0)
+        {
+            return Direction.Right;
+        }
+        else if (moveHorizontal < 0)
+        {
+            return Direction.Left;
+        }
+        else if (moveVertical > 0)
+        {
+            return Direction.Up;
+        }
+        else if (moveVertical < 0)
+        {
+            return Direction.Down;
+        } else
+            return Direction.Stoped;
+        {
+
+        }
     }
 }
