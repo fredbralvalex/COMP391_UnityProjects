5ca4cf42fb021ed33a69db09467aa2d7f8f06bd0
Frederico Alexandre <fred.bralvalex@gmail.com>
2018-03-04 23:55:15 -0500

    maze completes
    maze completed + pac man crazy movement

---
+++ Pac-Man Retro/Assets/PacManController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacManController : MonoBehaviour {
+
+
+    //Speed of PacMan and Phantoms
+    public const float SPEED_CONSTANT = 1f;
+
+    public float speed = 1;
+
+    private Rigidbody2D pacman;
+
+    void Start()
+    {
+        pacman = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate () {
+        //get move directions
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        pacman.AddForce(movement * speed * SPEED_CONSTANT);
+    }
+}
