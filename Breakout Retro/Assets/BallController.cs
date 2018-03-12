cd7d1a7a7631ef012d62cdb49e53633deaaf745a
Frederico Alexandre <fred.bralvalex@gmail.com>
2018-03-11 22:17:37 -0400

    End of a functional game
    End of a functional game - without life, without score

---
+++ Breakout Retro/Assets/BallController.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    private PaddleController paddleController;
+    private GameObject paddle;
+
+    /* Controls the ball movement   */
+    private float speed = 4f;
+    public bool stateStoped = true;
+    public bool stateLeft = true;
+    public bool stateUp = true;
+
+    private Vector2 direction;
+    // Use this for initialization
+    void Start () {
+        paddle = GameObject.Find("paddle");
+        paddleController = paddle.GetComponent<PaddleController>();
+    }    
+
+    void FixedUpdate() {
+        if (stateStoped)
+        {
+            transform.position = new Vector3 (paddle.transform.position.x, paddle.transform.position.y);
+        } else
+        {
+            moveUsingTransform();
+            move();
+        }
+    }
+
+    private void moveUsingTransform()
+    {
+        if (stateLeft)
+        {
+            direction.x = Vector2.left.x;
+        } else
+        {
+            direction.x = Vector2.right.x;
+        }
+
+        if (stateUp)
+        {
+            direction.y = Vector2.up.y;
+        } else
+        {
+            direction.y = Vector2.down.y;
+        }
+    }
+
+    private void move()
+    {
+        Vector2 newDirection = direction * speed * Time.deltaTime;                
+        transform.position += (Vector3)newDirection;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "paddle")
+        {         
+            effectByHitCollision(collision);
+            System.Console.WriteLine("HIT PADDLE");
+            stateUp = true;
+        }
+         
+        if (collision.gameObject.tag == "Brick")
+        {
+
+
+            System.Console.WriteLine("HIT BRICK");
+            Destroy(collision.gameObject.gameObject);
+        }        
+    }
+    void effectByHitCollision(Collision2D collision)
+    {
+        Collider2D collider = collision.collider;
+        Vector3 contactPoint = collision.contacts[0].point;
+        Vector3 center = collider.bounds.center;
+
+        bool right = contactPoint.x >= center.x;
+		bool left = contactPoint.x <= center.x;
+
+        if (stateLeft && right)
+        {
+            System.Console.Write("Effect Right");
+            stateLeft = false;
+            return;
+        }
+        if (!stateLeft && left)
+        {
+            System.Console.Write("Effect Left");
+            stateLeft = true;
+            return;
+        }       
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.name == "right wall")
+        {
+            stateLeft = true;
+            System.Console.WriteLine("HIT R WALL");
+        }
+        else if (collider.name == "left wall")
+        {
+            stateLeft = false;
+            System.Console.WriteLine("HIT L WALL");
+        }
+        else if (collider.name == "top wall")
+        {
+            stateUp = false;
+            System.Console.WriteLine("HIT T WALL");
+        }
+        else if (collider.name == "botton wall")
+        {
+            stateUp = true;
+            System.Console.WriteLine("HIT B WALL");
+            //TODO Must lose one chance
+            paddleController.InitialPosition();
+        }
+    }
+}
---
+++ Breakout Retro/Assets/BrickController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickController : MonoBehaviour {
+
+    private GameObject ball;
+    private BallController ballController;
+
+    void Start () {
+        ball = GameObject.Find("ball");
+        ballController = ball.GetComponent<BallController>();   
+    }
+	
+	
+	void FixedUpdate() {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Collider2D collider = collision.collider;
+        float RectWidth = GetComponent<Collider2D>().bounds.size.x;
+        float RectHeight = GetComponent<Collider2D>().bounds.size.y;
+        //float circleRad = collider.bounds.size.x;
+
+        if (collision.contacts.Length < 1)
+            return;
+
+        Vector3 contactPoint = collision.contacts[0].point;
+        Vector3 center = collider.bounds.center;
+
+        if (contactPoint.y > center.y && //checks that circle is on top of rectangle
+            (contactPoint.x < center.x + RectWidth / 2 && contactPoint.x > center.x - RectWidth / 2))
+        {
+            
+            System.Console.WriteLine("HIT TOP");
+            ballController.stateUp = false;
+        }
+        else if (contactPoint.y < center.y &&
+            (contactPoint.x < center.x + RectWidth / 2 && contactPoint.x > center.x - RectWidth / 2))
+        {
+           
+            System.Console.WriteLine("HIT BOTTOM");
+            ballController.stateUp = true;
+        }
+        else if (contactPoint.x > center.x &&
+            (contactPoint.y < center.y + RectHeight / 2 && contactPoint.y > center.y - RectHeight / 2))
+        {
+            
+            System.Console.WriteLine("HIT RIGHT");
+            ballController.stateLeft = true;
+        }
+        else if (contactPoint.x < center.x &&
+            (contactPoint.y < center.y + RectHeight / 2 && contactPoint.y > center.y - RectHeight / 2))
+        {
+            
+            System.Console.WriteLine("HIT LEFT");
+            ballController.stateLeft = false;
+        }
+        
+    }
+
+}
