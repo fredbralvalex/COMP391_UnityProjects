60a197809e87e7a9e40a5e64d8f2d4869ebfdc69
Frederico Alexandre <fred.bralvalex@gmail.com>
2018-03-18 01:11:38 -0400

    Release Medium
    Releasae Medium

--- Breakout Retro/Assets/BallController.cs
+++ Breakout Retro/Assets/BallController.cs
@@ -5,20 +5,32 @@ using UnityEngine;
 public class BallController : MonoBehaviour {
 
     private PaddleController paddleController;
+    private GameController gameController;
     private GameObject paddle;
+    private GameObject game;
 
     /* Controls the ball movement   */
     private float speed = 4f;
+    private float var = 1;
     public bool stateStoped = true;
     public bool stateLeft = true;
     public bool stateUp = true;
+
+    int redHits = 0;
+    int brownHits = 0;
+    int greenHits = 0;
+    int yellowHits = 0;
+
 
     private Vector2 direction;
     // Use this for initialization
     void Start () {
         paddle = GameObject.Find("paddle");
+        game = GameObject.Find("Canvas");
         paddleController = paddle.GetComponent<PaddleController>();
-    }    
+        gameController = game.GetComponent<GameController>();
+
+    }
 
     void FixedUpdate() {
         if (stateStoped)
@@ -51,8 +63,8 @@ public class BallController : MonoBehaviour {
     }
 
     private void move()
-    {
-        Vector2 newDirection = direction * speed * Time.deltaTime;                
+    { 
+        Vector2 newDirection = direction * speed * Time.deltaTime * var;                
         transform.position += (Vector3)newDirection;
     }
 
@@ -67,12 +79,56 @@ public class BallController : MonoBehaviour {
          
         if (collision.gameObject.tag == "Brick")
         {
+            string nameSprit = collision.gameObject.GetComponent<SpriteRenderer>().name;
+            //Debug.Log(collision.gameObject.GetComponent<SpriteRenderer>());
 
+            //Debug.Log(nameSprit);
+            if (nameSprit.Contains("yellow"))
+            {
+                gameController.points += 1;
+                yellowHits += 1;
+                //Debug.Log("yellow");
 
-            System.Console.WriteLine("HIT BRICK");
+            }
+            else if (nameSprit.Contains("green"))
+            {
+                gameController.points += 3;
+                greenHits += 1;
+                //Debug.Log("green");
+            } else if (nameSprit.Contains("brown"))
+            {
+                gameController.points += 5;
+                brownHits += 1;
+                //Debug.Log("brown");
+            } else if (nameSprit.Contains("red"))
+            {
+                gameController.points += 7;
+                yellowHits += 1;
+                //Debug.Log("red");
+            }
+            gameController.SetScoreText();
+            
             Destroy(collision.gameObject.gameObject);
+
+            int sumHits = yellowHits + greenHits + brownHits + redHits;
+            if (sumHits== 4 || sumHits == 12 || brownHits==1 || redHits==1)
+            {
+                increaseSpeed();
+            }
+           
         }        
     }
+
+    //after four hits, after twelve hits, and after making contact with the orange and red rows
+    void increaseSpeed()
+    {
+        var += 0.2f;
+    }
+    void shrinkSize()
+    {
+
+    }
+
     void effectByHitCollision(Collision2D collision)
     {
         Collider2D collider = collision.collider;
@@ -112,13 +168,16 @@ public class BallController : MonoBehaviour {
         {
             stateUp = false;
             System.Console.WriteLine("HIT T WALL");
+            shrinkSize();
         }
         else if (collider.name == "botton wall")
         {
             stateUp = true;
             System.Console.WriteLine("HIT B WALL");
             //TODO Must lose one chance
+            //shrink to half
             paddleController.InitialPosition();
+            gameController.removeLife();
         }
     }
 }
